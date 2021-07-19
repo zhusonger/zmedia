025ce88d83780ae452eaa8e28f94caa0d46f6176
zhusong <song.zhu@lasong.com.cn>
2021-07-19 18:27:06 +0800

    更新插件版本

--- src/main/java/cn/com/lasong/media/record/audio/AdapterAudioListener.java
+++ src/main/java/cn/com/lasong/media/record/audio/AdapterAudioListener.java
@@ -7,6 +7,45 @@ package cn.com.lasong.media.record.audio;
  * Description:
  * 适配回调, 避免实现太多的接口, 代码显的冗余
  */
-public class AdapterAudioListener {
+public class AdapterAudioListener implements IAudioListener {
 
+    @Override
+    public void onInitialized() {
+
+    }
+
+    @Override
+    public void onInitFail() {
+
+    }
+
+    @Override
+    public void onInterrupted() {
+
+    }
+
+    @Override
+    public void onStartFail() {
+
+    }
+
+    @Override
+    public void onPause() {
+
+    }
+
+    @Override
+    public void onResume() {
+
+    }
+
+    @Override
+    public void onSuccess() {
+
+    }
+
+    @Override
+    public boolean onFinish() {
+        return false;
+    }
 }
